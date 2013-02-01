da39acc96ff4134ec0af0ced43fcaa361246640c
vsxed <eduardmayer.mail@gmail.com>
2013-02-01 22:15:03 +0100

    Final Commit.

--- animation.js
+++ animation.js
@@ -5,20 +5,42 @@ $(document).ready(function () {
           		$anim3 = $('#animation-3');
 
           		$anim1.click(function () {
-          			$('.right').toggleClass('wink').delay(3500).queue(function() {  // Wait for it...
+          			$('.right').toggleClass('wink').delay(3001).queue(function() {  // Wait for it...
             			$(this).removeClass('wink').dequeue(); // aaaand it's gone!
        			});
-			$('.head').toggleClass('rotate').delay(3500).queue(function() {  // Wait for it...
+			$('.head').toggleClass('rotate').delay(3001).queue(function() {  // Wait for it...
             			$(this).removeClass('rotate').dequeue(); // aaaand it's gone!
        			});
           		});
 
           		$anim2.click(function () {
-          			$('.right').toggleClass('flag-right r-flaggy').delay(5500).queue(function() {  // Wait for it...
+          			$('.right').toggleClass('flag-right r-flaggy').delay(4001).queue(function() {  // Wait for it...
             			$(this).removeClass('flag-right r-flaggy').dequeue(); // aaaand it's gone!
        			});
-			$('.left').toggleClass('flag-left l-flaggy').delay(5500).queue(function() {  // Wait for it...
+			$('.left').toggleClass('flag-left l-flaggy').delay(4001).queue(function() {  // Wait for it...
             			$(this).removeClass('flag-left l-flaggy').dequeue(); // aaaand it's gone!
        			});
           		});
+
+              $anim3.click(function () {
+      //           $('.right').toggleClass('r-balls').delay(4500).queue(function() {  // Wait for it...
+      //             $(this).removeClass('r-balls').dequeue(); // aaaand it's gone!
+      //       });
+      // $('.left').toggleClass('l-balls').delay(4500).queue(function() {  // Wait for it...
+      //             $(this).removeClass('l-balls').dequeue(); // aaaand it's gone!
+      //       });
+      $('.leg').toggleClass('anim').delay(6050).queue(function() {  // Wait for it...
+                  $(this).removeClass('anim').dequeue(); // aaaand it's gone!
+            });
+      $('.eyes').toggleClass('anim').delay(6200).queue(function() {  // Wait for it...
+                  $(this).removeClass('anim').dequeue(); // aaaand it's gone!
+            });
+      $('.arms').toggleClass('moon').delay(6100).queue(function() {  // Wait for it...
+                  $(this).removeClass('moon').dequeue(); // aaaand it's gone!
+            });
+      $('.android').toggleClass('moonwalk').delay(6500).queue(function() {  // Wait for it...
+                  $(this).removeClass('moonwalk').dequeue(); // aaaand it's gone!
+            });
+
+              });
 });
